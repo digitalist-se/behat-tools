29dee207d7b4b0d59a3cfcf5a495ca4433803c52
Jose Jiménez <jose.jimenez@digitalistgroup.com>
2024-10-01 10:40:51 +0200

    Create CleanUpContext.php

---
+++ src/Context/CleanUpContext.php
@@ -0,0 +1,38 @@
+<?php
+
+use Behat\Gherkin\Node\TableNode;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class CleanUpContext extends RawDrupalContext {
+
+  private EntityTypeManagerInterface $entityTypeManager;
+
+  public function __construct() {
+    $this->entityTypeManager = \Drupal::entityTypeManager();
+  }
+
+  /**
+   * Given I delete the content of the entity "user" with the properties:
+   * | mail                |
+   * | license-01@test.com |
+   * | license-02@test.com |
+   *
+   * @Given /^I delete the content of the entity "([^"]*)" with the properties:$/
+   */
+  public function iDeleteTheContentOfTheEntityWithTheProperties($entity_type, TableNode $table) {
+    $rows = $table->getHash();
+    foreach ($rows as $row) {
+      $entities = $this->entityTypeManager->getStorage($entity_type)
+        ->loadByProperties($row);
+      foreach ($entities as $entity) {
+        $entity->delete();
+      }
+    }
+  }
+
+}
